9aa89ba45d83b76ff4bf588fc3419f3063b8248d
DevTKSS <info@technische-konstruktion.com>
2025-08-01 15:12:29 +0200

    feat(OpenIddict): Add sample to test if that works for current uno version
    So far that does not work, so its not used at the moment

---
+++ src/OpenIddict.Client.UnoIntegrations/UnoHostApplicationLifetime.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
+
+namespace OpenIddict.Sandbox.UnoClient;
+//https://github.com/Ecierge/openiddict-core/blob/apple-cryptokit/sandbox/OpenIddict.Sandbox.Uno.Client/UnoHostApplicationLifetime.cs
+public class UnoHostApplicationLifetime : ApplicationLifetime, IHostApplicationLifetime
+{
+    public UnoHostApplicationLifetime(ILogger<ApplicationLifetime> logger) : base(logger) { }
+
+    CancellationToken IHostApplicationLifetime.ApplicationStarted => this.ApplicationStarted;
+
+    CancellationToken IHostApplicationLifetime.ApplicationStopping => this.ApplicationStopping;
+
+    CancellationToken IHostApplicationLifetime.ApplicationStopped => this.ApplicationStopped;
+
+    void IHostApplicationLifetime.StopApplication()
+    {
+        this.StopApplication();
+        Environment.Exit(0);
+    }
+}
+
+public class ActivationHostApplicationLifetime : ApplicationLifetime
+{
+    public ActivationHostApplicationLifetime(ILogger<ApplicationLifetime> logger) : base(logger)
+    {
+        this.ApplicationStarted.Register(() => this.StopApplication());
+    }
+}
