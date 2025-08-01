40a263f07a85231f8608e31fdfe66d4303d1ddf3
DevTKSS <info@technische-konstruktion.com>
2025-08-01 15:13:48 +0200

    chore: Add different converters from Uno.Chefs Sample that might be usefull in the future

---
+++ src/DevTKSS.MyManufacturerERP/Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,29 @@
+//using DevTKSS.MyManufacturerERP.Client.Models;
+using Microsoft.UI.Xaml.Data;
+
+namespace DevTKSS.MyManufacturerERP.Converters;
+
+//public class TimeSpanToStringConverter : IValueConverter
+//{
+//	public object Convert(object value, Type targetType, object parameter, string language)
+//	{
+//		if (value is TimeSpanObject tso)
+//		{
+//			var ts = new TimeSpan(tso.Ticks ?? 0);
+
+//			var ftm = string.Format("{0} {1}",
+//				ts.Hours > 0 ? ts.ToString(@"%h' hour.'") : string.Empty,
+//				ts.Minutes > 0 ? ts.ToString(@"%m' mins'") : string.Empty);
+
+//			return ftm;
+
+//		}
+
+//		return string.Empty;
+//	}
+
+//	public object ConvertBack(object value, Type targetType, object parameter, string language)
+//	{
+//		throw new NotImplementedException();
+//	}
+//}
