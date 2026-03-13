cfd3bebd072194abfe58ad3acf1a07ec0342b0bc
DevTKSS <info@technische-konstruktion.com>
2026-03-13 15:10:57 +0100

    chore(AuthorizationState): move and add comments

--- src/Extensions/OAuth/AuthorizationState.cs
+++
@@ -1,9 +0,0 @@
-namespace DevTKSS.Extensions.OAuth;
-
-public class AuthorizationState
-{
-    public string? State { get; set; }
-    public string? CodeVerifier { get; set; }
-    public string? StartUrl { get; set; }
-    public string? RedirectUri { get; set; }
-}
